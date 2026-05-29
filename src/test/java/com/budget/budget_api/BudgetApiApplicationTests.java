03116f1a9c6d6f20df61ae8bb8ce33c3a29e317e
maru <marustalmach@student.agh.edu.pl>
2026-05-29 14:13:46 +0200

    init Spring Boot proj setup

---
+++ src/test/java/com/budget/budget_api/BudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.budget_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
