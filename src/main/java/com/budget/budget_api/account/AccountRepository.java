292137c65898dc649feb33cc728cc039b3e896b8
maru <marustalmach@student.agh.edu.pl>
2026-05-30 13:05:31 +0200

    defined repositories for transaction and account

---
+++ src/main/java/com/budget/budget_api/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.budget.budget_api.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
