78936efae5ae217d97e7459fef5a098776474acb
maru <marustalmach@student.agh.edu.pl>
2026-05-30 17:19:04 +0200

    added deletingTransaction feature

--- src/main/java/com/budget/budget_api/transaction/TransactionController.java
+++ src/main/java/com/budget/budget_api/transaction/TransactionController.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -36,6 +34,12 @@ public class TransactionController {
     public ResponseEntity<TransactionResponse> createTransaction(@Valid @RequestBody TransactionRequest request) {
         TransactionResponse savedTransaction = transactionService.createNewTransaction(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTransaction);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
+        transactionService.deleteTransaction(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
