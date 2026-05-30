0afb7ed6887fbf8db5d4de00055401df8012bbe1
maru <marustalmach@student.agh.edu.pl>
2026-05-30 17:24:00 +0200

    added classes and records connected with summarising and aggregating info

---
+++ src/main/java/com/budget/budget_api/summary/SummaryController.java
@@ -0,0 +1,8 @@
+package com.budget.budget_api.summary;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SummaryController {
+
+}
---
+++ src/main/java/com/budget/budget_api/summary/SummaryService.java
@@ -0,0 +1,11 @@
+package com.budget.budget_api.summary;
+
+import com.budget.budget_api.transaction.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SummaryService {
+
+    private TransactionRepository transactionRepository;
+
+}
