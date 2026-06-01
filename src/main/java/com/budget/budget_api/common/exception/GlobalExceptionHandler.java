ad90b6dee5aa391a348d241bb9f32406e1372236
MaruStalmach <marustalmach@student.agh.edu.pl>
2026-06-01 14:32:00 +0200

    deleted unused imports

--- src/main/java/com/budget/budget_api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/budget/budget_api/common/exception/GlobalExceptionHandler.java
@@ -3,12 +3,9 @@ package com.budget.budget_api.common.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
