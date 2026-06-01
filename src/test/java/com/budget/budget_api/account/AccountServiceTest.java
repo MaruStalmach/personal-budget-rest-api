ed0710e3be8e887348bb39fbcce91529fc7cad8c
maru <marustalmach@student.agh.edu.pl>
2026-06-01 22:57:13 +0200

    added test files

--- src/test/java/com/budget/budget_api/account/AccountServiceTest.java
+++ src/test/java/com/budget/budget_api/account/AccountServiceTest.java
@@ -1,21 +1,73 @@
 package com.budget.budget_api.account;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
 
+    @Mock
+    private AccountRepository accountRepository;
+    @InjectMocks
+    private  AccountService accountService;
+
     @Test
-    void getAllAccounts() {
+    void deleteAccountById() {
+
+        Long accountId = 1L;
+        Account mockAccount = new Account();
+        mockAccount.setId(accountId);
+        mockAccount.setName("mock account");
+        mockAccount.setBalance(BigDecimal.ZERO);
+        mockAccount.setTransactions(new ArrayList<>());
+
+        when(accountRepository.findById(accountId))
+                .thenReturn(Optional.of(mockAccount));
+
+        accountService.deleteAccountById(accountId);
+
+        verify(accountRepository, times(1)).delete(mockAccount);
+
+
     }
 
     @Test
+    void deleteAccountById_ShouldDelete_WhenNoTransactions() {
+
+    }
+
+
+
+    @Test
     void getAccountById() {
+
+        Long accountId = 1L;
+        Account mockAccount = new Account();
+        mockAccount.setId(accountId);
+        mockAccount.setName("mock account");
+        mockAccount.setBalance(BigDecimal.ZERO);
+        mockAccount.setTransactions(new ArrayList<>());
+
+
+        when(accountRepository.findById(accountId))
+                .thenReturn(Optional.of(mockAccount));
+
+        verify(accountRepository, times(1)).delete(mockAccount);
     }
 
     @Test
     void createNewAccount() {
+        mock()
     }
 
     @Test
