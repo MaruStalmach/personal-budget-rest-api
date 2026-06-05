516c7bd8e28fe103a3c62f07d1eccec2b0928bc4
MaruStalmach <marustalmach@student.agh.edu.pl>
2026-06-05 09:23:34 +0200

    added a fieldErrors to ErrorResponse

--- src/main/java/com/budget/budget_api/common/exception/ErrorResponse.java
+++ src/main/java/com/budget/budget_api/common/exception/ErrorResponse.java
@@ -1,11 +1,13 @@
 package com.budget.budget_api.common.exception;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public record ErrorResponse(
         LocalDateTime timestamp,
         int status,
         String error,
         String message,
-        String path
+        String path,
+        Map<String, String> fieldErrors
 ) {}
